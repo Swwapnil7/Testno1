389dea1b51df45b82a8e4c58f358c8259da617fc
sachindada2506 <sachin.patil7557@gmail.com>
2022-02-20 19:01:16 +0530

    tp

--- UpstoxBuySell/src/main/java/pro/upstox/Tp.java
+++ UpstoxBuySell/src/main/java/pro/upstox/Tp.java
@@ -1,5 +1,24 @@
 package pro.upstox;
 
 public class Tp {
+	
+	public static void main(String[] args) {
+		
+		String a = "malayalam";
+		int len = a.length();
+		String rev ="";
+		for(int i=len-1;i>=0;i--) 
+		{
+		 rev= rev + a.charAt(i);
+		}
+		if(rev.equals(a)==true) 
+		{
+			System.out.println("String is palendrom");
+		}
+		else 
+		{
+			System.out.println("String is not a palendrom");
+		}
+	}
 
 }
